845fa67353671f63c02ff0cefe50955ade1395e6
robinlrx <robin@monkey-lab.io>
2023-02-05 20:51:59 +0100

    add twig page + readme

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-"# symfony" 
+in ``.env.local`` replace `root` by your username to connect to mysql
+
+### Import data from api :
+`php bin/console data:import --entity=[entity]`
+
+replace `[entity]` by one of the following :
+- starships 
+- films
+- people
+- planets
+- species
+- vehicles
+
+### show dashboard
+``http://127.0.0.1:8000/admin``
+
+
+
 
 
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use App\Entity\People;
 use App\Entity\Planets;
 use App\Entity\Species;
 use App\Entity\Starships;
+use App\Entity\User;
 use App\Entity\Vehicles;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -19,12 +20,12 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
+        //return parent::index();
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
-        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        return $this->redirect($adminUrlGenerator->setController(PeopleCrudController::class)->generateUrl());
+        //$adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        //return $this->redirect($adminUrlGenerator->setController(PeopleCrudController::class)->generateUrl());
 
         // Option 2. You can make your dashboard redirect to different pages depending on the user
         //
@@ -35,7 +36,7 @@ class DashboardController extends AbstractDashboardController
         // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
         // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
         //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        return $this->render('templates/admin/my-custom-page.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -53,5 +54,6 @@ class DashboardController extends AbstractDashboardController
         yield MenuItem::linkToCrud('Species', 'fas fa-list', Species::class);
         yield MenuItem::linkToCrud('Starships', 'fas fa-list', Starships::class);
         yield MenuItem::linkToCrud('Vehicles', 'fas fa-list', Vehicles::class);
+        yield MenuItem::linkToCrud('Users', 'fas fa-list', User::class);
     }
 }
